2fbfc22163e142f30a4cc300fbdc2b1b429227b3
meggs1 <megan.schertz@gmail.com>
2020-11-03 13:59:04 -0600

    commit tree 3c4bac89de6b69bdcbec210c6c628c699c9c981f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :words
+
+def initialize(words)
+    @words = words
+end
+
+def match(string)
+    string.select do |word|
+        word.split("").sort == words.split("").sort
+    end
+
+end
+
+end
